82a76de04aaf6225e063295ba4453d66a32bdaa7
Tunc Polat <tunc.polat@simplweb.ch>
2021-12-02 09:46:22 +0100

    Add proxy tests

--- test/Proposal.js
+++ test/Proposal.js
@@ -22,4 +22,21 @@ contract("Proposal", async accounts => {
     const manager = await campaign.manager();
     assert.strictEqual(accounts[0], manager);
   });
+
+  it("assigns equal amounts of tokens", async () => {
+    let amountAccountOne = await campaign.balanceOf(accounts[0])
+    let amountAccountTwo = await campaign.balanceOf(accounts[0])
+    assert.strictEqual(amountAccountOne.words[0], 1)
+    assert.strictEqual(amountAccountTwo.words[0], 1)
+  }) 
+
+  it("transfers proxy vote", async () => {
+    await campaign.transfer(accounts[1], "1") // transfer votes
+    let amountAccountOne = await campaign.balanceOf(accounts[0])
+    let amountAccountTwo = await campaign.balanceOf(accounts[1])
+    assert.strictEqual(amountAccountOne.words[0], 0)
+    assert.strictEqual(amountAccountTwo.words[0], 2)
+  })
+
+  
 });
